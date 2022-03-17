cb8c4bde9f463615d200e66c73903151c4d6b285
Jose Silva <mcvicksilvaa@gmail.com>
2022-03-17 08:30:45 -0500

    fix landing page

--- frontend/src/components/Pages/LandingPage.tsx
+++ frontend/src/components/Pages/LandingPage.tsx
@@ -7,7 +7,7 @@ import { Page } from "./Page";
 const useStyles = makeStyles((theme: any) => ({
     hero: {
         minHeight: '100vh',
-        backgroundImage: 'url(https://i.imgur.com/rN99GjR.jpg)',
+        backgroundImage: 'url(https://i.imgur.com/MVdRJJT.png)',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover'
     }
@@ -21,30 +21,30 @@ export const LandingPage: React.FC = () => {
                 marginTop: '200px'
             }}>
                 <Typography
-                    variant='h1'
+                    variant='h2'
                     sx={{
-                        marginTop: '300px',
-                        color: 'white',
+                        marginTop: '100px',
+                        color: COLORS.light_red,
                         textAlign: "center",
                         "@media (max-width: 1200px)": {
                             margin: "-50px auto 5px auto",
                             fontSize: "clamp(2.5rem, 10vw, 6rem)",
-                            width: "90vw"
+                            width: "70vw"
                         }
                     }}>
                     All your menus in one place
                 </Typography>
                 <Typography
-                    variant="h3"
+                    variant="h4"
                     sx={{
                         marginTop: "30px",
-                        color: COLORS.cyan,
+                        color: 'gray',
                         textAlign: "center",
                         "@media (max-width: 1200px)": {
                             marginLeft: "auto",
                             marginRight: "auto",
                             fontSize: "clamp(2rem, 10vw, 4rem)",
-                            width: "90vw"
+                            width: "80vw"
                         }
                     }}
                 >
@@ -53,7 +53,7 @@ export const LandingPage: React.FC = () => {
             </Box>
             <div id="_overlay"></div>
         </Page>
-);
+    );
 }
 
 export default LandingPage;
